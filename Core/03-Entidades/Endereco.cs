04ef65da4471366483fcfcb7ae7d839a6ebb1f28
Eduarda <163910410+duda-1@users.noreply.github.com>
2025-05-15 15:57:40 -0300

    Adicionar arquivos de projeto.

---
+++ Core/03-Entidades/Endereco.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core._03_Entidades
+{
+    class Endereco
+    {
+        public int IdEndereco { get; set; }          // PRIMARY KEY, AUTO_INCREMENT
+        public string CEP { get; set; }              // VARCHAR(8)
+        public int? TipoVia { get; set; }            // INT(1), pode ser NULL
+        public string NomeVia { get; set; }          // VARCHAR(45)
+        public string Numero { get; set; }           // VARCHAR(5)
+        public string Complemento { get; set; }      // VARCHAR(45)
+        public string Bairro { get; set; }           // VARCHAR(45)
+        public string Cidade { get; set; }           // VARCHAR(45)
+        public string UF { get; set; }               // VARCHAR(2)
+    }
+}
